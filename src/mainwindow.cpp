a2d1c2622de38a86ac891cb9a6c5fc298e4caea5
ag-10 <albert.groma10@gmail.com>
2021-04-16 20:06:57 +0200

    First design

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QGraphicsLineItem"
+#include "QGraphicsEllipseItem"
+#include "QGraphicsTextItem"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    auto *scene = new QGraphicsScene(ui->graphicsView);
+    ui->graphicsView->setScene(scene);
+
+    auto line = scene->addLine(150,150,50,30);
+    line->setPen(QPen({Qt::red}, 3));
+    line->setFlag(QGraphicsItem::ItemIsSelectable);
+    line->setFlag(QGraphicsItem::ItemIsMovable);
+
+    scene->addEllipse(150,150,50,30);
+
+    auto text = scene->addText("Ahoj svet");
+    text->setTextInteractionFlags(Qt::TextEditorInteraction);
+
+
+
 }
 
 MainWindow::~MainWindow()
